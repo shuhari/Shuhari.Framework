3acce700401279b81bc6e278b292172a18e5f745
yuhao <shuhari@outlook.com>
2017-01-11 17:06:25 +0800

    Add mapping

--- Shuhari.Framework.Data/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Shuhari.Framework.Data")]
-[assembly: AssemblyDescription("ORM and helpers for Relational database")]
-[assembly: Guid("06333fa1-d425-4115-bcb3-07c6cbb8336a")]
